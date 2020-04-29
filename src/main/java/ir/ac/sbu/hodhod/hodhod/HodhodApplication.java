ce028ace65467f942f48872da77e066bcab470b2
mohammad <mamadazarani@gmail.com>
2020-04-29 14:47:48 +0430

    start hodhod project- spring bot

---
+++ src/main/java/ir/ac/sbu/hodhod/hodhod/HodhodApplication.java
@@ -0,0 +1,13 @@
+package ir.ac.sbu.hodhod.hodhod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HodhodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HodhodApplication.class, args);
+    }
+
+}
