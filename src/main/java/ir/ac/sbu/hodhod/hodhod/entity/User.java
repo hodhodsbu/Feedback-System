85f5a84c492a8ce1535e7872dd0967396f91cca8
mohammad <mamadazarani@gmail.com>
2020-05-06 16:12:42 +0430

    User.java: Add lombok, add NonNull annotation

--- src/main/java/ir/ac/sbu/hodhod/hodhod/entity/User.java
+++ src/main/java/ir/ac/sbu/hodhod/hodhod/entity/User.java
@@ -2,6 +2,8 @@ package ir.ac.sbu.hodhod.hodhod.entity;
 
 
 import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -9,25 +11,31 @@ import java.util.Set;
 
 @Data
 @Entity
+@RequiredArgsConstructor
 public class User {
     @Id
     @GeneratedValue
     private Long id;
 
+    //    @RequiredTypes()
+    @NonNull
     @Column(unique = true, nullable = false)
     private String email;
 
+    @NonNull
     private String fullName;
+
+    @NonNull
+    private String password;
+
 
     @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinTable(
-            name="users_roles",
-            joinColumns = {@JoinColumn(name="user_id")},
+            name = "users_roles",
+            joinColumns = {@JoinColumn(name = "user_id")},
             inverseJoinColumns = {@JoinColumn(name = "role_id")}
     )
     private Set<Role> roles = new HashSet<>();
 
-
-    
 
 }
