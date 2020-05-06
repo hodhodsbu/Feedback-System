14a8756849f7b787c8a640659046a3c135d0eaca
mohammad <mamadazarani@gmail.com>
2020-05-06 16:11:28 +0430

    modify Role

--- src/main/java/ir/ac/sbu/hodhod/hodhod/entity/Role.java
+++ src/main/java/ir/ac/sbu/hodhod/hodhod/entity/Role.java
@@ -1,13 +1,21 @@
-package ir.ac.sbu.hodhod.hodhod.entity
+package ir.ac.sbu.hodhod.hodhod.entity;
 
-import lombok.Data
+import lombok.Data;
 
-import javax.persistence.Entity
-import javax.persistence.Table
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Entity
-@Table(name = "roles")
+@Table(name= "roles")
 public class Role {
+    @Id
+    @GeneratedValue
+    private Long id;
 
+    private String role;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
 }
