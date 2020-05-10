c5aafc27601c3045d2c442eb36686f4c6b660a74
mohammad <mamadazarani@gmail.com>
2020-05-10 13:05:20 +0430

    Suggestion Repository

---
+++ src/main/java/ir/ac/sbu/hodhod/hodhod/repositories/SuggestionRepository.java
@@ -0,0 +1,10 @@
+package ir.ac.sbu.hodhod.hodhod.repositories;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface SuggestionRepository extends PagingAndSortingRepository {
+
+}
